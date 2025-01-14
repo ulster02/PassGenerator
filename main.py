2de4706549db61a83bb9696d480677d648ac26f9
ulster02 <torresc515@gmail.com>
2025-01-13 22:47:39 -0600

    Generacion de contraseña basada en cant de dígitos

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import Password
 import pymongo
 import random
 import string
+import sys
 
 x=0
 alfa = string.ascii_letters + string.digits
 
 while x == 0 :
     #random_character = random.choice(alfa)
-    print(random.choice(alfa))
+    #print(random.choice(alfa))
+    random_character = ""
+    cantDig = int(input("De cuantos dígitos quieres tu contraseña?\n"))
+    for i in range( cantDig):
+        random_character = random.choice(alfa) + random_character
+    print(random_character)
     x = int(input("quieres seguir? 1/0 \n"))
